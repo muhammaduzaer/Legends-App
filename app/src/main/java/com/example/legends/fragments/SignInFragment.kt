6d6cc877c47248b2fa95c1fb229acb632b8322ff
muhammaduzair <muhammaduzaer@gmail.com>
2020-11-10 17:23:30 +0500

    With Firebase auth.

--- app/src/main/java/com/example/legends/fragments/SignInFragment.kt
+++ app/src/main/java/com/example/legends/fragments/SignInFragment.kt
@@ -1,6 +1,7 @@
 package com.example.legends.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,8 +22,10 @@ class SignInFragment : Fragment(), View.OnClickListener {
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         ButtonSignIn.setOnClickListener(this)
         textViewSignUp.setOnClickListener(this)
     }
--- app/src/main/java/com/example/legends/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/legends/fragments/SignUpFragment.kt
@@ -1,34 +1,26 @@
 package com.example.legends.fragments
 
+import android.content.ContentValues.TAG
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.legends.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_sign_in.*
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+import kotlinx.android.synthetic.main.fragment_sign_up.editTextEmailField
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SignUpFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +30,35 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        ButtonRegister.setOnClickListener(this)
+        textViewSignIn.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            ButtonRegister.id -> {
+
+                val email = editTextEmail.text.toString()
+                val password = editTextPassword.text.toString()
+
+                Log.d("SignInFragment", "Email is:"+ email)
+                Log.d("SignInFragment", "Password is: $password")
+
+                //firebase auth to create a user with email and password
+
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(requireContext(), "Please enter email and password to log in.", Toast.LENGTH_SHORT).show()
+                    return
                 }
-            }
+                findNavController().navigate(R.id.action_signUpFragment_to_homeFragment)
+                }
+            textViewSignIn.id -> findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
     }
 }
